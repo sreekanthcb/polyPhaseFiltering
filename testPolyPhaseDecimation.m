6976434a26bf6e62993612d8ebef5233bf2017e5
sreekanthcb <sreekanthcb@yahoo.com>
2025-04-18 11:52:11 +0530

    Cosmetic changes

--- testPolyPhaseDecimation.m
+++ testPolyPhaseDecimation.m
@@ -16,17 +16,23 @@ out4    = downsample(filter(b,1,out1),4);
 
 % Filter and downsampling
 figure(1);
-subplot(3,1,1);spectrumVisualizer(out1,1e6);
-subplot(3,1,2);spectrumVisualizer(out2,0.5*1e6);
-subplot(3,1,3);spectrumVisualizer(out4,0.25*1e6);
+subplot(3,1,1);
+spectrumVisualizer(out1,1e6);
+subplot(3,1,2);
+spectrumVisualizer(out2,0.5*1e6);
+subplot(3,1,3);
+spectrumVisualizer(out4,0.25*1e6);
 
 % Polyphase Filtering
 out2PolyPhase = PolyPhaseDecimator(b,2,out1);
 out4PolyPhase = PolyPhaseDecimator(b,4,out1);
 figure(2);
-subplot(3,1,1);spectrumVisualizer(out1,1e6);
-subplot(3,1,2);spectrumVisualizer(out2PolyPhase,0.5*1e6);
-subplot(3,1,3);spectrumVisualizer(out4PolyPhase,0.25*1e6);
+subplot(3,1,1);
+spectrumVisualizer(out1,1e6);
+subplot(3,1,2);s
+pectrumVisualizer(out2PolyPhase,0.5*1e6);
+subplot(3,1,3);
+spectrumVisualizer(out4PolyPhase,0.25*1e6);
 
 figure(3)
 subplot(2,1,1)
